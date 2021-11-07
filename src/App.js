ee19cc5c5c93431bb325ee63147457f95b18183f
pedrodruviaro <pedrodruviaro@gmail.com>
2021-11-06 22:58:25 -0300

    desafio useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
+const Produto = ({ produto }) => {
+    return (
+        <article>
+            <h4>Produto: {produto.nome}</h4>
+            <h4>Preço: R$ {produto.preco}</h4>
+            <h4>Descrição: {produto.descricao}</h4>
+            <h4>Vendido: {produto.vendido ? "Não" : "Sim"}</h4>
+        </article>
+    );
+};
+
 export const App = () => {
+    const [produto, setProduto] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    const buscaProduto = async (e) => {
+        setLoading(true);
+        const busca = e.target.innerText.toLowerCase();
+        const response = await fetch(
+            `https://ranekapi.origamid.dev/json/api/produto/${busca}`
+        );
+        const data = await response.json();
+        setProduto(data);
+        setLoading(false);
+    };
+
     return (
         <>
-            <h1>Finalizando modulo 2</h1>
+            <h1>React Hooks</h1>
+            <div>
+                <button onClick={buscaProduto}>Tablet</button>
+                <button onClick={buscaProduto}>Smartphone</button>
+                <button onClick={buscaProduto}>Notebook</button>
+            </div>
+
+            {loading && <h3>Buscando produto...</h3>}
+            {produto && !loading && <Produto produto={produto} />}
         </>
     );
 };
